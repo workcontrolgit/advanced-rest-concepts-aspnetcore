41cf88ce3c7a4ee29ef4ad81e4a9336c561a56f1
fuji.nguyen <fuji.nguyen@workcontrol.com>
2020-12-14 23:22:50 -0500

    before migrate owner/account fake

---
+++ Entities/Helpers/FakeData.cs
@@ -0,0 +1,41 @@
+﻿using Bogus;
+using Bogus.Extensions;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Helpers
+{
+    static class FakeData
+    {
+        public static List<Owner> Owners = new List<Owner>();
+        public static List<Account> Accounts = new List<Account>();
+
+        public static void Init(int count)
+        {
+            var accountFaker = new Faker<Account>()
+               .RuleFor(p => p.Id, f => Guid.NewGuid())
+               .RuleFor(p => p.AccountType, f => f.Commerce.ProductMaterial())
+               .RuleFor(p => p.DateCreated, f => f.Date.Recent());
+
+            var ownerFaker = new Faker<Owner>()
+               .RuleFor(b => b.Id, f => Guid.NewGuid())
+               .RuleFor(b => b.Name, f => f.Name.FullName())
+               .RuleFor(b => b.Id, (f, b) =>
+               {
+                   accountFaker.RuleFor(p => p.OwnerId, _ => b.Id);
+
+                   var accounts = accountFaker.GenerateBetween(3, 5);
+
+                   FakeData.Accounts.AddRange(accounts);
+
+                   return default; // Owner.Accounts is a getter only. The return value has no impact.
+               });
+
+            var owners = ownerFaker.Generate(count);
+
+            FakeData.Owners.AddRange(owners);
+        }
+    }
+}
--- Entities/RepositoryContext.cs
+++ Entities/RepositoryContext.cs
@@ -1,4 +1,5 @@
 ﻿using Bogus;
+using Entities.Helpers;
 using Entities.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,14 +13,18 @@ namespace Entities
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-        var owner = new Faker<Owner>()
-        .RuleFor(m => m.Id, f => f.Random.Guid())
-        .RuleFor(m => m.Name, f => f.Name.FullName())
-        .RuleFor(m => m.Address, f => f.Address.FullAddress())
-        .RuleFor(m => m.DateOfBirth, f => f.Date.Past());
-        
-        modelBuilder.Entity<Owner>()
-            .HasData(owner.Generate(1000));
+            //var owner = new Faker<Owner>()
+            //.RuleFor(m => m.Id, f => f.Random.Guid())
+            //.RuleFor(m => m.Name, f => f.Name.FullName())
+            //.RuleFor(m => m.Address, f => f.Address.FullAddress())
+            //.RuleFor(m => m.DateOfBirth, f => f.Date.Past());
+
+            //modelBuilder.Entity<Owner>()
+            //    .HasData(owner.Generate(1000));
+            FakeData.Init(10);
+
+            modelBuilder.Entity<Owner>().HasData(FakeData.Owners);
+            modelBuilder.Entity<Account>().HasData(FakeData.Accounts);
         }
         public DbSet<Owner> Owners { get; set; }
         public DbSet<Account> Accounts { get; set; }
