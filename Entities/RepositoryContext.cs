d4a55eb8a1d69ebe3c0138d92d919b0889647bfa
fuji.nguyen <fuji.nguyen@workcontrol.com>
2020-12-14 22:20:29 -0500

    Prepare bogus

--- Entities/RepositoryContext.cs
+++ Entities/RepositoryContext.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Models;
+﻿using Bogus;
+using Entities.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Entities
@@ -9,7 +10,16 @@ namespace Entities
             :base(options)
         {
         }
-
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        var owner = new Faker<Owner>()
+        .RuleFor(m => m.Id, f => f.Random.Guid())
+        .RuleFor(m => m.Name, f => f.Name.FullName())
+        .RuleFor(m => m.Address, f => f.Address.FullAddress())
+        .RuleFor(m => m.DateOfBirth, f => f.Date.Past());
+            modelBuilder.Entity<Owner>()
+                .HasData(owner.Generate(1000));
+        }
         public DbSet<Owner> Owners { get; set; }
         public DbSet<Account> Accounts { get; set; }
 	}
